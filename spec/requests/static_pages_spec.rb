6620040aeed875d8d3ea6cedf9643daa4819ccf4
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2013-10-02 16:33:36 +0700

    Finish Contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
+    end
+
   end
 
   describe "Help page" do
@@ -24,5 +32,19 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
   end
 end
