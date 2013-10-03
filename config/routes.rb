992636b28b1575b9ce6464dda43a4ddc6c94ea61
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2013-10-03 12:02:48 +0700

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/home',    to: 'static_pages#home',    via: 'get'
   #get "static_pages/home"
   match '/help',    to: 'static_pages#help',    via: 'get'
